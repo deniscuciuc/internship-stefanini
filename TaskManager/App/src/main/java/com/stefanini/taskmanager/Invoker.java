63d032227ea29b3e95a0e3242e54da1f3219a032
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-06-27 14:30:14 +0300

    Removed constructor. Added javadoc comments

--- TaskManager/App/src/main/java/com/stefanini/taskmanager/Invoker.java
+++ TaskManager/App/src/main/java/com/stefanini/taskmanager/Invoker.java
@@ -2,17 +2,27 @@ package com.stefanini.taskmanager;
 
 import service.Command;
 
+/**
+ * This is invoker class that will set and execute commands
+ * @author dcuciuc
+ *
+ */
 public class Invoker {
 	Command action;
 	
-	public Invoker() {
-		
-	}
 	
+	/**
+	 * Set the command class for Invoker
+	 * @param command
+	 */
 	public void setCommand(Command command) {
 		action = command;
 	}
 	
+	
+	/**
+	 * Execute current command
+	 */
 	public void executeCommand() {
 		action.execute();
 	}
