79840fa0100c28a9c79e9325717e7959fbeb5031
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-06-27 14:31:51 +0300

    Changed return of methods from lists to void

--- TaskManager/DaoService/src/main/java/dao/TaskDAO.java
+++ TaskManager/DaoService/src/main/java/dao/TaskDAO.java
@@ -1,11 +1,10 @@
 package dao;
 
-import java.util.List;
 
 import domain.TaskEntity;
 import domain.UserEntity;
 
 public interface TaskDAO {
 	void addTask(UserEntity user, TaskEntity task);
-	List<TaskEntity> getAllTasks(UserEntity user);
+	void getAllTasks(UserEntity user);
 }
--- TaskManager/DaoService/src/main/java/dao/UserDAO.java
+++ TaskManager/DaoService/src/main/java/dao/UserDAO.java
@@ -1,10 +1,10 @@
 package dao;
 
-import java.util.List;
+
 
 import domain.UserEntity;
 
 public interface UserDAO {
 	void createUser(UserEntity user);
-	List<UserEntity> getAllUsers(List<UserEntity> users);
+	void getAllUsers();
 }
