1956169f55e63cd3dace781ed4b37cefa792cdeb
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-06-27 14:30:46 +0300

    Added public modifier to class

--- TaskManager/DaoService/src/main/java/dao/DAOFactory.java
+++ TaskManager/DaoService/src/main/java/dao/DAOFactory.java
@@ -2,7 +2,7 @@ package dao;
 
 import dao.enums.AvaibleDAOFactories;
 
-abstract class DAOFactory {
+public abstract class DAOFactory {
 	public abstract UserDAO getUserDAO();
 	public abstract TaskDAO getTaskDAO();
 	
