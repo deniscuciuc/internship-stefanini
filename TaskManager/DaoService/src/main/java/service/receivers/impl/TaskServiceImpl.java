11406dd6838f56cbc3d1b2c8cea3e57bdfa2a2e3
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-07-11 07:00:03 +0300

    Added JavaDoc and realization

--- TaskManager/DaoService/src/main/java/service/receivers/impl/TaskServiceImpl.java
+++ TaskManager/DaoService/src/main/java/service/receivers/impl/TaskServiceImpl.java
@@ -1,33 +1,55 @@
 package service.receivers.impl;
 
 
-import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 
+import domain.UserEntity;
+import domain.TaskEntity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import dao.DAOFactory;
 import dao.enums.AvailableDAOFactories;
-import domain.entities.TaskEntity;
 import service.receivers.TaskService;
+import service.receivers.exceptions.InvalidUserException;
 
+/**
+ * This class represents all command implementations for tasks operations.
+ * Calling by concrete commands methods executes all operations with databases and client UI
+ * @author dcuciuc
+ */
 public class TaskServiceImpl implements TaskService {
 	
-	private static final Logger logger = LogManager.getLogger(TaskServiceImpl.class);  
-	
+	private static final Logger logger = LogManager.getLogger(TaskServiceImpl.class);
+
+	/**
+	 * This method should generate task and save it in database.
+	 * Method takes the username from the console, then gets the user from the database.
+	 * Then asks for all task details from the console and sets user to the task
+	 * After all method save task in database
+	 */
 	@Override
 	public void addTask() {
+		String userName = getUserNameFromConsole();
+		UserEntity user = getUserByUserNameHibernateMySQL(userName);
+
 		TaskEntity task = getTaskDataFromConsole();
+		task.setUser(user);
+
 		saveTaskHibernateMySQLByUserId(task);
 	}
-	
+
+	/**
+	 * Method prints all user's tasks in console.
+	 * Once the method has requested a username from the console,
+	 * it retrieves the user's tasks from the database by username and displays them from the set of
+	 */
 	@Override
 	public void showAllUsersTasks() {
 		String userName = getUserNameFromConsole();
-		List<TaskEntity> tasks = getUsersTasksByUserNameHibernateMySQL(userName);
-		tasks.stream().forEach(taskEntity -> System.out.println(taskEntity.toString()));
+		Set<TaskEntity> tasks = getUsersTasksByUserNameHibernateMySQL(userName);
+		displayTasksConsole(userName, tasks);
  	}
 
 
@@ -44,14 +66,37 @@ public class TaskServiceImpl implements TaskService {
 	}
 
 	private String getUserNameFromConsole() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Username: ");
-		String userName = scanner.nextLine();
-		return userName;
+		try {
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("Username: ");
+			String userName = scanner.nextLine();
+
+			checkIfUserWithSuchUserNameExists(userName);
+
+			return userName;
+		} catch (InvalidUserException e) {
+			System.out.println(e);
+		}
+		return null;
+	}
+	private void displayTasksConsole(String userName, Set<TaskEntity> tasks) {
+		System.out.println("User: " + userName + " have " + tasks.size() + " task/tasks");
+		tasks.stream().forEach(taskEntity -> System.out.println(taskEntity.toString()));
 	}
 
-	private List<TaskEntity> getUsersTasksByUserNameHibernateMySQL(String userName) {
-		return DAOFactory.getDAOFactory(AvailableDAOFactories.HIBERNATE).getTaskDAO().getAllUsersTasks(userName);
+	private void checkIfUserWithSuchUserNameExists(String userName) throws InvalidUserException {
+		UserEntity user = DAOFactory.getDAOFactory(AvailableDAOFactories.HIBERNATE).getUserDAO().getUserByUserName(userName);
+		if (user == null) {
+			throw new InvalidUserException("The user with this name does not exist!");
+		}
+	}
+
+	private Set<TaskEntity> getUsersTasksByUserNameHibernateMySQL(String userName) {
+		return DAOFactory.getDAOFactory(AvailableDAOFactories.HIBERNATE).getUserDAO().getUserByUserName(userName).getTasks();
+	}
+
+	private UserEntity getUserByUserNameHibernateMySQL(String userName) {
+		return DAOFactory.getDAOFactory(AvailableDAOFactories.HIBERNATE).getUserDAO().getUserByUserName(userName);
 	}
 
 	private void saveTaskHibernateMySQLByUserId(TaskEntity task) {
--- TaskManager/DaoService/src/main/java/service/receivers/impl/UserServiceImpl.java
+++ TaskManager/DaoService/src/main/java/service/receivers/impl/UserServiceImpl.java
@@ -4,24 +4,36 @@ import java.util.*;
 
 import dao.DAOFactory;
 import dao.enums.AvailableDAOFactories;
-import domain.beans.TaskBean;
-import domain.beans.UserBean;
-import domain.entities.TaskEntity;
-import domain.entities.UserEntity;
+import domain.TaskEntity;
+import domain.UserEntity;
 import service.receivers.UserService;
+import service.receivers.exceptions.InvalidUserException;
 
-
+/**
+ * This class represents all command implementations for user operations.
+ * Calling by concrete commands methods executes all operations with databases and client UI
+ * @author dcuciuc
+ */
 public class UserServiceImpl implements UserService {
-	
+
+	/**
+	 * Method gets user details from console, generates user object, then save it in database
+	 */
 	@Override
 	public void createUser() {
 		UserEntity user = getUserDataFromConsole();
 		saveUserHibernateMySQL(user);
 	}
 
+	/**
+	 * Method gets user and tasks details from console, generate both objects, then save user in database
+	 */
 	@Override
 	public void createUserWithTask() {
 		UserEntity user = getUserDataFromConsole();
+
+		if (user == null) return;
+
 		TaskEntity task = getTaskDataFromConsole();
 
 		task.setUser(user);
@@ -30,29 +42,38 @@ public class UserServiceImpl implements UserService {
 		saveUserHibernateMySQL(user);
 	}
 
+	/**
+	 * Method shows / prints all users' information by calling them from database
+	 */
 	@Override
 	public void showAllUsers() {
 		List<UserEntity> users = getAllUsersFromMySQLHibernate();
-		users.stream().forEach(userEntity -> System.out.println(userEntity.toString()));
+		displayUsersConsole(users);
 	}
 
-	@SuppressWarnings("resource")
 	private UserEntity getUserDataFromConsole() {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Username: ");
-		String userName = scanner.nextLine();
+		try {
+			Scanner scanner = new Scanner(System.in);
 
-		System.out.print("First name: ");
-		String firstName = scanner.nextLine();
+			System.out.print("Username: ");
+			String userName = scanner.nextLine();
 
-		System.out.print("Last name: ");
-		String lastName = scanner.nextLine();
+			checkIfUserNameAlreadyExists(userName);
+
+			System.out.print("First name: ");
+			String firstName = scanner.nextLine();
+
+			System.out.print("Last name: ");
+			String lastName = scanner.nextLine();
 
 
-		Set<TaskEntity> tasks = new HashSet<TaskEntity>();
+			Set<TaskEntity> tasks = new HashSet<TaskEntity>();
 
-		return new UserEntity(firstName, lastName, userName, tasks);
+			return new UserEntity(firstName, lastName, userName, tasks);
+		} catch (InvalidUserException e) {
+			System.out.println(e);
+		}
+		return null;
 	}
 
 	private TaskEntity getTaskDataFromConsole() {
@@ -65,6 +86,17 @@ public class UserServiceImpl implements UserService {
 		String description = scanner.nextLine();
 
 		return new TaskEntity(title, description);
+	}
+
+	private void checkIfUserNameAlreadyExists(String userName) throws InvalidUserException {
+		UserEntity user = DAOFactory.getDAOFactory(AvailableDAOFactories.HIBERNATE).getUserDAO().getUserByUserName(userName);
+		if (user != null) {
+			throw new InvalidUserException("Username already exists!");
+		}
+	}
+
+	private void displayUsersConsole(List<UserEntity> users) {
+		users.stream().forEach(userEntity -> System.out.println(userEntity.toString()));
 	}
 
 	private void saveUserHibernateMySQL(UserEntity user) {
